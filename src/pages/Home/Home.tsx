8d6b4daa141c1623edbe6e80c945141b96b00d2a
JaniTech2025 <suba.suri@gmail.com>
2025-06-30 11:47:00 +1000

    Adds changes to image path

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,17 +2,19 @@ import classes from './Home.module.scss';
 import {CarouselDisplay} from "../../Components/Carousel/CarouselDisplay";
 import FavouritedProducts from '../../Components/FavouritedProducts/FavouritedProducts';
 import ProductsGrid from '../../Components/ProductsGrid/ProductsGrid';
+const base = import.meta.env.BASE_URL;
+
 
 export default function Home() {
 
   
-  const carouselimages: string[] = [
-    "./public/images/lifestyle1.jpg",
-    "./public/images/lifestyle2.jpg",
-    "./public/images/lifestyle3.jpg",
-    "./public/images/lifestyle4.jpg",
-    "./public/images/lifestyle5.jpg",
-   ];
+const carouselimages: string[] = [
+  `${base}images/lifestyle1.jpg`,
+  `${base}images/lifestyle2.jpg`,
+  `${base}images/lifestyle3.jpg`,
+  `${base}images/lifestyle4.jpg`,
+  `${base}images/lifestyle5.jpg`,
+];
 
   return (
     <div className={classes.container}>
